3a8ab265c28212455c8b5005a9b32b480e929be2
jason <jason.caplin@gmail.com>
2014-10-30 18:55:51 +0000

    whatever

---
+++ xcaplin/99bottles.rb
@@ -0,0 +1,10 @@
+def sing_the_song(bottles_number)
+	while bottles_number > 0
+		puts "#{bottles_number} bottles of beer on the wall, etc etc"
+		bottles_number -=1
+	end
+	puts "No more bottles."
+end
+
+bottles=99
+sing_the_song(bottles)
