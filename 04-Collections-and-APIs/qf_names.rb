894f87d376585d7efa307ac80f95560acc1907f7
Łukasz Korecki <lukasz@coffeesounds.com>
2014-11-03 22:51:27 +0000

    Add lesson 04

---
+++ 04-Collections-and-APIs/qf_names.rb
@@ -0,0 +1,8 @@
+# List processing
+#
+# Write a method which accepts an array of names as an argument and
+# returns a list of names and their associated lengths.
+
+# Write a second method which accepts array returned by first one
+# and prints out each name and its length. If the name is longer than 6
+# characters the method needs to print out "Wow, that's a long name!"
